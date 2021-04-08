fb7027b83c5ec4f9e42960249a52435fcb0b1972
WEIJIAN LI <leevi1044@outlook.com>
2021-04-08 13:58:27 -0500

    trying to make the training run in GPU

--- RNN.py
+++ RNN.py
@@ -10,7 +10,7 @@ import numpy as np
 is_cuda = torch.cuda.is_available()
 
 if is_cuda:
-    device = torch.device("cuda")
+    device = torch.device("cpu")
     print("GPU is available")
 else:
     device = torch.device("cpu")
